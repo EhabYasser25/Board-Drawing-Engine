ea1d7caf224ad36a3bb46c311980472eaf3e90eb
Ehab Yasser <eng.ehabyasser@gmail.com>
2023-05-06 12:47:10 +0300

    small update

--- functional_scala/src/main/scala/AbstractGameEngine.scala
+++ functional_scala/src/main/scala/AbstractGameEngine.scala
@@ -10,21 +10,20 @@ def AbstractGameEngine(
 }
 
 @tailrec
-private def play(board: Array[Array[Char]],
+private def play(
+                  board: Array[Array[Char]],
                  controller: (Array[Array[Char]], String, Boolean) => (Array[Array[Char]], Boolean),
                  drawer:  Array[Array[Char]] => Unit,
                  twoPlayersGame: Boolean,
-                 player1Turn: Boolean): Unit = {
+                 player1Turn: Boolean
+                ): Unit = {
   if (twoPlayersGame) println(f"Player ${if (player1Turn) 1 else 2} - Turn")
-  scala.io.StdIn.readLine() match {
-    case input =>
-      controller(board, input, player1Turn) match {
-        case (_,false) =>
-          println(s"Invalid Move, Try again.")
-          play(board, controller, drawer, twoPlayersGame, player1Turn)
-        case (newBoard,true) =>
-          drawer(newBoard)
-          play(newBoard, controller, drawer, twoPlayersGame, !player1Turn)
-      }
+  controller(board, scala.io.StdIn.readLine(), player1Turn) match {
+    case (_,false) =>
+      println(s"Invalid Move, Try again.")
+      play(board, controller, drawer, twoPlayersGame, player1Turn)
+    case (newBoard,true) =>
+      drawer(newBoard)
+      play(newBoard, controller, drawer, twoPlayersGame, !player1Turn)
   }
 }
